83b2a23d21bedc4bf16e541dbf34c9486c617f4c
XuYangjie <xu42@XuYangjiedeMacBook-Pro.local>
2017-03-15 16:44:16 +0800

    修复因学信网改变规则导致查询失败的bug

--- src/CetScore.php
+++ src/CetScore.php
@@ -5,22 +5,29 @@ namespace Xu42\Cet;
 class CetScore
 {
     /**
-     * @param string zkzh 准考证号
-     * @param string xm 姓名
-     * @return string 请求的网址
+     * 查询四六级成绩
+     * @param string $number 准考证号
+     * @param string $name 姓名
+     * @return array
      */
-    private function setUrl($zkzh, $xm)
+    public function get($number, $name)
     {
-        $url = null;
-        if (is_numeric($zkzh) && (strlen($zkzh) === 15) && is_string($xm)) {
-            $url = 'http://www.chsi.com.cn/cet/query?zkzh='.$zkzh.'&xm='.urlencode($xm);
+        if (is_null($number) || is_null($name)) {
+            return null;
         }
-        
-        return $url;
+        $url = $this->setUrl($number, $name);
+        $webPage = $this->getWebPage($url);
+        return $this->cetScore($webPage);
+    }
+
+
+    private function setUrl($number, $name)
+    {
+        return 'http://www.chsi.com.cn/cet/query?zkzh=' . $number . '&xm=' . urlencode($name);
     }
 
     /**
-     * @param string url 请求的网址
+     * @param string $url 请求的网址
      * @return string 包含成绩信息的网页源码
      */
     private function getWebPage($url)
@@ -41,8 +48,8 @@ class CetScore
     }
 
     /**
-     * @param string webPage 包含成绩信息的网页源码
-     * @return string 个人和四六级成绩等信息的数组（该数组尚不完美）
+     * @param string $webPage 包含成绩信息的网页源码
+     * @return array 个人和四六级成绩等信息的数组（该数组尚不完美）
      */
     private function cetScore($webPage)
     {
@@ -53,44 +60,35 @@ class CetScore
         preg_match_all('/<table(.|\s)*?<\/table>/', $webPage, $matches);
         preg_match_all('/(>)(.|\s)*?(<)/', $matches[0][1], $matches);
 
-        $search = array('<','>','：', chr(13).chr(10));
+        $search = ['<', '>', '：', chr(13) . chr(10)];
         $result = str_replace($search, '', $matches[0]);
 
-        $content = array();
         foreach ($result as $value) { //去除 数组value 前后的空格
             $content[] = trim($value);
         }
 
-        $content = array_filter($content); // 删除数组空元素
-
-        $cetScore = array();
-        foreach ($content as $value) { // 数组key重新排序
+        foreach (array_filter($content) as $value) { // 数组key重新排序
             $cetScore[] = $value;
         }
 
-        $score = null;
-        if (!isset($cetScore[11])) {
-            return $score;
-        }
-        for ($i=1; $i<count($cetScore); $i+=2) {
-            $score[] = $cetScore[$i];
-        }
+        $score = [
+            'name' => (string)$cetScore[3],
+            'school' => (string)$cetScore[7],
+            'type' => (string)$cetScore[9],
+            'written' => [
+                'number' => (string)$cetScore[12],
+                'score' => (int)$cetScore[16],
+                'listening' => (int)$cetScore[20],
+                'reading' => (int)$cetScore[24],
+                'translation' => (int)$cetScore[26]
+            ],
+            'oral' => [
+                'number' => (string)$cetScore[29],
+                'score' => (string)$cetScore[33]
+            ]
+        ];
+
         return $score;
     }
 
-    /**
-     * 查询四六级成绩
-     * @param string zkzh 准考证号
-     * @param string xm 姓名
-     * @return array
-     */
-    public function get($zkzh, $xm)
-    {
-        if (is_null($zkzh) || is_null($xm)) {
-            return null;
-        }
-        $url = $this->setUrl($zkzh, $xm);
-        $webPage = $this->getWebPage($url);
-        return $this->cetScore($webPage);
-    }
 }
